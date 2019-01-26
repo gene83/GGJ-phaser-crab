3eafc3c15155355a325c2af5a7a25f943430c08d
Gene83 <gene83@protonmail.com>
2019-01-26 11:40:56 -1000

    add sprite movement and animation

--- beach.js
+++ beach.js
@@ -4,30 +4,60 @@ class Beach extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('crab', 'assets/crab.png');
+    this.load.spritesheet('crab', 'assets/crab.png', {
+      frameWidth: 250,
+      frameHeight: 200
+    });
   }
 
   create() {
-    this.image = this.add.image(300, 500, 'crab');
+    this.player = this.physics.add.sprite(300, 400, 'crab');
+    this.player.setCollideWorldBounds(true);
 
-    this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers('crab', {
+        start: 0,
+        end: 1
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.key_UP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.key_LEFT = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.LEFT
+    );
+    this.key_DOWN = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
+    );
+    this.key_RIGHT = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.RIGHT
+    );
   }
 
   update(delta) {
-    if (this.key_W.isDown) {
-      this.image.y--;
-    }
-    if (this.key_A.isDown) {
-      this.image.x--;
-    }
-    if (this.key_S.isDown) {
-      this.image.y++;
-    }
-    if (this.key_D.isDown) {
-      this.image.x++;
+    if (this.key_UP.isDown) {
+      this.player.setVelocityY(-160);
+
+      this.player.anims.play('walk', true);
+    } else if (this.key_LEFT.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play('walk', true);
+    } else if (this.key_DOWN.isDown) {
+      this.player.setVelocityY(160);
+
+      this.player.anims.play('walk', true);
+    } else if (this.key_RIGHT.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play('walk', true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.setVelocityY(0);
+
+      this.player.anims.play('walk', false);
     }
   }
 }
