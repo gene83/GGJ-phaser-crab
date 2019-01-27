5d2534c95eb728d731e87afd410e6bb923050f06
Brad McKinney <brad.mckin@gmail.com>
2019-01-26 18:11:31 -1000

    updates plankton sprite and adds a load progress bar

--- beach.js
+++ beach.js
@@ -4,6 +4,28 @@ class Beach extends Phaser.Scene {
   }
 
   preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    this.load.on('progress', (value) => {
+      console.log(value);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', (file) => {
+      console.log(file.src);
+    });
+
+    this.load.on('complete', () => {
+      console.log('complete');
+      progressBar.destroy();
+      progressBox.destroy();
+    });
+
     this.load.image('bg', 'assets/background-sand.png');
     this.load.spritesheet('crab', 'assets/crab.png', {
       frameWidth: 100,
